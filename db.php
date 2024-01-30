877772df6c60b01c7332d70221cab84e66988521
guillealcuaz <57131152+guillealcuaz@users.noreply.github.com>
2024-01-30 18:07:24 +0100

    db.php
    Primera versión para conectar la BD con nuestro servidor web

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "root"; 
+$password = "";
+$dbname = "gpslogin"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+$conn->set_charset("utf8");
