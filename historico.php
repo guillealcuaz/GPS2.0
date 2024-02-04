5e9201bf0368261d49537bfa15020d9a42ef8f7b
guillealcuaz <57131152+guillealcuaz@users.noreply.github.com>
2024-02-04 20:43:24 +0100

    historico.php con sesiones

--- historico.php
+++ historico.php
@@ -1,12 +1,23 @@
 <?php
-$misgeoJSON = "misgeoJSON/";
-$subidos = scandir($misgeoJSON);
+include 'db.php';
 
-echo "<ul>";
-foreach ($subidos as $archivo) {
-    if ($archivo != "." && $archivo != "..") {
-        echo "<li><a href='" . $misgeoJSON . $archivo . "'>" . $archivo . "</a></li>";
+if (isset($_SESSION['id_usuario'])) {
+    $idUsuario = $_SESSION['id_usuario'];
+
+    $consulta = $conn->prepare("SELECT archivo FROM archivos_subidos WHERE id_usuario = ?");
+    $consulta->bind_param('i', $idUsuario); 
+    $consulta->execute();
+    $resultado = $consulta->get_result();
+    $carpeta = 'misgeoJSON/';
+
+    echo "<ul>";
+    while ($fila = $resultado->fetch_assoc()) {
+        $ruta = $carpeta . $fila['archivo'];
+        echo "<li><a href='" . $ruta . "'>" . $fila['archivo'] . "</a></li>"; 
     }
+    echo "</ul>";
+    $consulta->close();
+} else {
+    echo "Inicia sesión para ver tus archivos.";
 }
-echo "</ul>";
 ?>
